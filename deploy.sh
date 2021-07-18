5af7863714ebd7208f957a8cd5af4a89eaec8979
ej <crazy979797@gmail.com>
2021-07-18 18:42:39 +0800

    modify deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 set -e
 
 # build
-npm run build
+yarn build
 
 # navigate into the build output directory
 cd dist
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:chenej9797/vue3-todolist.git main:gh-pages
+git push -f git@github.com:chenej9797/vue3-todolist.git master:gh-pages
 
 cd -
